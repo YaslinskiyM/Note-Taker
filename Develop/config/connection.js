1c45d63a3f384f08bcc9a8c5b960f1b5aaa4733e
maria <mariayaslinskiy@gmail.com>
2023-07-06 08:04:44 -0700

    removed unnecessary code

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -13,25 +13,3 @@ const sequelize = new Sequelize(
 );
 
 module.exports = sequelize;
-
-
-
-
-
-
-
-//const Sequelize = require('sequelize');
-
-//require('dotenv').config();
-
-//const sequelize = process.env.JAWSDB_URL
-  //? new Sequelize(process.env.JAWSDB_URL)
-  //: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    //  host: 'localhost',
-      //dialect: 'mysql',
-      //dialectOptions: {
-       // decimalNumbers: true,
-      //},
-    //});
-
-//module.exports = sequelize; 
